8a32d46887743c71474a69d352118c51ffccbe0a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-11 10:24:29 -0400

    Se diseñó el sidebar

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components';
 import { TopMenu } from '../../components/ui/top-menu/TopMenu';
 
 export default function ShopLayout({
@@ -8,6 +9,7 @@ export default function ShopLayout({
   return (
     <main className="min-h-screen">
       <TopMenu />
+      <Sidebar />
 
       <div className="px-0 sm:px-10">
         { children }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './products/product-grid/ProductGridItem';
 export * from "./ui/title/Title";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/top-menu/TopMenu";
+export * from './ui/sidebar/Sidebar';
