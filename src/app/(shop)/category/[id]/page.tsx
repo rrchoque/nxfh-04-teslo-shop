2d7dab22ce698745a2df57f73622a69397e57ee1
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-12 08:46:31 -0400

    Se crearon las páginas de las categorías

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,25 +1,45 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   },
   searchParams: {
     page?: string; 
   }
 }
 
+const seedProducts = initialData.products;
+
 export default function ({ params }: Props) {
 
   const {id} = params;
 
-  if (id === 'kids') {
-    notFound()
+  const products = seedProducts.filter( product => product.gender === id )
+
+  const label: Record<Category, string> = {
+    'men': 'hombres',
+    'women': 'mujeres',
+    'kid': 'niños',
+    'unisex': 'todos'
   }
 
+  // if (id === 'kids') {
+  //   notFound()
+  // }
+
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={ `Artículos para ${ label[id] }` }
+        subtitle={`Productos de la categoría ${ (label as any)[id] }`} 
+        className="mb-2"
+       />
+
+       <ProductGrid products={products} />
+    </>
   );
 }
