05cf2bd5512aa146a34496209b343a1e78ed606b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-12 10:10:44 -0400

    Se creó la página de producto con slideshow

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,11 @@
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
+
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideshow';
+export * from './product/slideshow/ProductMobileSlideshow';
+
 export * from "./ui/title/Title";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/top-menu/TopMenu";
