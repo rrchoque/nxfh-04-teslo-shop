b743bf7dd1dc3e509d9fa8411ac2315fc9c1b219
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-26 05:24:14 -0400

    Se creó el procedimiento para la creación de nuevos usuarios

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 export { authenticate } from './auth/login';
 export { logout } from './auth/logout';
+export { registerUser } from './auth/register';
 
 export { getProductBySlug } from './product/get-product-by-slug';
 export { getStockBySlug } from './product/get-stock-by-slug'
