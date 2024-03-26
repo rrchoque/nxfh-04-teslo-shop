e6dc34d18daec83661079966261da351f4968e83
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-26 05:45:44 -0400

    Se validaron las rutas de checkout solo para usuarios autenticados

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -22,6 +22,19 @@ export const authConfig: NextAuthConfig  = {
             session.user = token.data as any;
             return session;
         },
+
+        authorized({ auth, request: { nextUrl } }) {
+            console.log({auth})
+            // const isLoggedIn = !!auth?.user;
+            // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            // if (isOnDashboard) {
+            //   if (isLoggedIn) return true;
+            //   return false; // Redirect unauthenticated users to login page
+            // } else if (isLoggedIn) {
+            //   return Response.redirect(new URL('/dashboard', nextUrl));
+            // }
+            return true;
+        },
     },
     providers: [
         credentials({
