6433b32d6896cd4b05a8895acdc2c0ebe2dbc471
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-03 08:05:25 -0400

    Se creó el skeleton de los botones de paypal

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 
 interface Props {
   orderId: string;
@@ -10,8 +10,15 @@ interface Props {
 
 export const PayPalButton = ({ orderId, amount }: Props) => {
 
+  const [{ isPending }] = usePayPalScriptReducer();
+
   return (
-    <PayPalButtons 
-    />
+    <>
+        {isPending ? <div className="animate-pulse" >
+            <div className='h-11 bg-gray-200 rounded' />
+            <div className='h-11 bg-gray-200 rounded mt-4' />
+          </ div> : null}
+        <PayPalButtons />
+    </>
   )
 }
