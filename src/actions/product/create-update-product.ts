02b0b2dcf036aa0eb733e8287c1d3e33c33b24bd
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-11 08:28:44 -0400

    Se creó el transaction para actualizar el producto ACTUAL

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -35,6 +35,70 @@ export const createUpdateProduct = async( formData: FormData ) => {
     return { ok: false }
   }
 
-  console.log(productParsed.data)
-  return { ok: true }
+  const product = productParsed.data;
+  product.slug = product.slug.toLowerCase().replace(/ /g, '-' ).trim();
+
+  const { id, ...rest } = product;
+
+  try {
+    const prismaTx = await prisma.$transaction( async (tx) => {
+  
+      let product: Product;
+      const tagsArray = rest.tags.split(',').map( tag => tag.trim().toLowerCase() );
+  
+      if ( id ) {
+        // Actualizar
+        product = await prisma.product.update({
+          where: { id },
+          data: {
+            ...rest,
+            sizes: {
+              set: rest.sizes as Size[],
+            },
+            tags: {
+              set: tagsArray
+            }
+          }
+        });
+  
+      } else {
+        // Crear
+        product = await prisma.product.create({
+          data: {
+            ...rest,
+            sizes: {
+              set: rest.sizes as Size[],
+            },
+            tags: {
+              set: tagsArray
+            }
+          }
+        })
+      }
+      
+      return {
+        product
+      }
+    });
+
+
+    // Todo: RevalidatePaths
+    revalidatePath('/admin/products');
+    revalidatePath(`/admin/product/${ product.slug }`);
+    revalidatePath(`/products/${ product.slug }`);
+
+
+    return {
+      ok: true,
+      product: prismaTx.product,
+    }
+
+    
+  } catch (error) {
+    
+    return {
+      ok: false,
+      message: 'Revisar los logs, no se pudo actualizar/crear'
+    }
+  }
 }
